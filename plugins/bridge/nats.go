6e20eb22f5e2193c4809b1f4f4ca40e13f9922ea
hxx <yellowstar520@gmail.com>
2021-03-20 11:05:42 +0800

    使用 stan 模式，支持消息缓存

--- plugins/bridge/nats.go
+++ plugins/bridge/nats.go
@@ -5,18 +5,20 @@ import (
 	"fmt"
 	"io/ioutil"
 
-	"github.com/nats-io/nats.go"
+	"github.com/nats-io/stan.go"
 	"go.uber.org/zap"
 )
 
 type natsConfig struct {
-	Addr             string          `json:"addr"`
-	Topic     string            `json:"topic"`
+	Addr      string `json:"addr"`
+	ClusterId string `json:"cluster_id"`
+	ClientId  string `json:"client_id"`
+	Topic     string `json:"topic"`
 }
 
 type natsClient struct {
 	natsConfig natsConfig
-	natsConn nats.Conn
+	natsConn   stan.Conn
 }
 
 //Init init nats client
@@ -24,13 +26,22 @@ func InitNats() *natsClient {
 	log.Info("start connect nats....")
 	content, err := ioutil.ReadFile("./plugins/bridge/nats/nats.json")
 	if err != nil {
-		log.Fatal("Read config file error: ", zap.Error(err))
+		log.Fatal(
+			"Read config file error: ",
+			zap.Error(err),
+		)
 	}
 	// log.Info(string(content))
 	var config natsConfig
-	err = json.Unmarshal(content, &config)
+	err = json.Unmarshal(
+		content,
+		&config,
+	)
 	if err != nil {
-		log.Fatal("Unmarshal config file error: ", zap.Error(err))
+		log.Fatal(
+			"Unmarshal config file error: ",
+			zap.Error(err),
+		)
 	}
 	c := &natsClient{natsConfig: config}
 	c.connect()
@@ -39,22 +50,35 @@ func InitNats() *natsClient {
 
 //connect
 func (n *natsClient) connect() {
-	natsConn, err := nats.Connect(n.natsConfig.Addr)
+	sc, err := stan.Connect(n.natsConfig.ClusterId, fmt.Sprintf("hmq-%s", n.natsConfig.ClientId), stan.NatsURL(n.natsConfig.Addr))
 	if err != nil {
-		log.Fatal("create nats async producer failed: ", zap.Error(err))
+		log.Fatal(
+			"create nats async producer failed: ",
+			zap.Error(err),
+		)
 	}
 
-	n.natsConn = *natsConn
+	n.natsConn = sc
 }
 
 //Publish publish to nats
 func (n *natsClient) Publish(e *Elements) error {
-	log.Debug("element: ", zap.Any("element", e))
+	log.Debug(
+		"element: ",
+		zap.Any(
+			"element",
+			e,
+		),
+	)
 	switch e.Action {
 	case Publish:
 		if e.Topic != "" {
 			subj := e.Topic
-			if err := n.publish(subj, e.ClientID, e.Payload); err != nil {
+			if err := n.publish(
+				subj,
+				e.ClientID,
+				e.Payload,
+			); err != nil {
 				return err
 			}
 		}
@@ -70,7 +94,14 @@ func (n *natsClient) publish(topic string, clientId string, msg string) error {
 		return err
 	}
 
-	if err := n.natsConn.Publish(topic, []byte(fmt.Sprintf("%s: %s", clientId, payload))); err != nil {
+	if err := n.natsConn.Publish(
+		topic,
+		[]byte(fmt.Sprintf(
+			"%s: %s",
+			clientId,
+			payload,
+		)),
+	); err != nil {
 		return err
 	}
 
