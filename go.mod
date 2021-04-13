c52999fd358d60c0fbee814dc8eb81b60e9821c9
hxx <yellowstar520@gmail.com>
2021-04-13 10:17:55 +0800

    使用 mango.json 配置文件配置 mangodb 信息

--- go.mod
+++ go.mod
@@ -10,15 +10,17 @@ require (
 	github.com/eclipse/paho.mqtt.golang v1.2.0
 	github.com/gin-gonic/gin v1.4.0
 	github.com/google/uuid v1.1.1
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
 	github.com/nats-io/nats-server/v2 v2.2.0 // indirect
 	github.com/nats-io/nats-streaming-server v0.21.1 // indirect
 	github.com/nats-io/stan.go v0.8.3
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/segmentio/fasthash v0.0.0-20180216231524-a72b379d632e
-	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.6.1
 	github.com/tidwall/gjson v1.6.8
 	go.mongodb.org/mongo-driver v1.5.1
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110
 	google.golang.org/protobuf v1.26.0 // indirect
--- model/init.go
+++ model/init.go
@@ -1,26 +1,55 @@
 package model
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
-	_ "github.com/fhmq/hmq/config"
-	"github.com/fhmq/hmq/db/helper"
-	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.uber.org/zap"
+	"io/ioutil"
+	"log"
+)
+
+type Config struct {
+	Addr  string `json:"addr"`
+	Database   string `json:"database"`
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+var (
+	config Config
 )
 
 func init() {
-	var mongoOpt = helper.MongoOpt{
-		Addr:     viper.GetString("mongo.addr"),
-		UserName: viper.GetString("mongo.username"),
-		Password: viper.GetString("mongo.password"),
+	content, err := ioutil.ReadFile("./plugins/auth/authmango/mango.json")
+	if err != nil {
+		log.Fatal("Read config file error: ", zap.Error(err))
 	}
+	// log.Info(string(content))
 
-	//初始化mongo
-	if err := helper.NewMongo(mongoOpt); err != nil {
-		panic(fmt.Sprintf("初始化mongo客户端 err: %v", err))
+	err = json.Unmarshal(content, &config)
+	if err != nil {
+		log.Fatal("Unmarshal config file error: ", zap.Error(err))
 	}
+	// fmt.Println("http: config: ", config)
 
-	database := viper.GetString("mongo.database")
+	clientOptions := options.Client().ApplyURI(config.Addr)
+	//clientOptions.Auth = &options.Credential{Username: mo.UserName, Password: mo.Password}
+	// Connect to MongoDB
+	mongoDB, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal("mongo connect failed: ", zap.Error(err))
+	}
+	// Check the connection
+	if err = mongoDB.Ping(context.TODO(), nil); err != nil {
+		log.Fatal("mongo ping failed: ", zap.Error(err))
+	}
+	fmt.Println("mongoDB run success!")
+
+	database := config.Database
 
 	//初始化表格
-	AclColl = helper.MongoDB.Database(database).Collection(Acl{}.TableName())
+	AclColl = mongoDB.Database(database).Collection(Acl{}.TableName())
 }
